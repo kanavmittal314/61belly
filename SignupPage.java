469928ec65889656f450b3972fbce3654f5f5dc3
Kanav Mittal <kanavmittal@berkeley.edu>
2024-11-04 15:37:06 -0800

    signup page added

---
+++ SignupPage.java
@@ -0,0 +1,13 @@
+public class SignupPage {
+	private String username;
+	private String password;
+
+	public SignupPage(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+
+	public void addAccount() {
+		# TODO
+	}
+}
